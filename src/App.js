05930c9393ee9c5e7ec306d1f290fe66edafd1df
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-04-29 02:40:00 +0100

    welcome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+import Final from "./tutorial/1-useState/final/3-useState-array";
 
 function App() {
   return (
     <div className="container">
-      <Setup />
+      <Final />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,70 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { data } from "../../../data";
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [people, setPeople] = React.useState(data);
+  // const removeItem = (id) => {
+  //   const newPeople = people.filter((person) => person.id !== id);
+  //   setPeople(newPeople);
+  // };
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const handleClick = () => {
+    setPeople([]);
+  };
+  //using state with an object
+  const [peson, setPerson] = useState({
+    name: "Timothy",
+    age: "24",
+    message: "random message",
+  });
+  //another way using three seperate state
+  const [name, setName] = useState("Daniel");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
+  const changeMessage = () => {
+    setPerson({ ...peson, message: "hello world" });
+  };
+  const changedMessage = () => {
+    setMessage("Nigeria is my cocuntry");
+  };
+  return (
+    <>
+      <div className="container">
+        <h3>{peson.name}</h3>
+        <h3>{peson.age}</h3>
+        <h3>{peson.message}</h3>
+        <section>
+          <h3>{name}</h3>
+          <h3>{age}</h3>
+          <h3>{message}</h3>
+          <button className="btn" onClick={changedMessage}>
+            changedMessage
+          </button>
+        </section>
+        <button className="btn" onClick={changeMessage}>
+          changemessage
+        </button>
+      </div>
+      {people.map((person) => {
+        const { id, name } = person;
+        console.log(person);
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeItem(id)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={handleClick}>
+        clear all
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;

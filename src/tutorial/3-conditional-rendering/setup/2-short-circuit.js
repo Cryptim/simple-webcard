6d16e2c15fc53ff347b1bcd0c860bcb1e48cf1e9
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-02 01:14:50 +0100

    ternary

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [user, setUser] = useState(0);
+  const [isError, setIsError] = useState(false);
+  const [text, setText] = useState("Tims");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
+  console.log(secondValue);
+  function increase() {
+    setUser(user + 1);
+  }
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <div>
+        <h2>short circuiting {user}</h2>
+        <button
+          className="btn"
+          onClick={() => {
+            setIsError(!isError);
+          }}
+        >
+          Toggle error
+        </button>
+        {/* {!isError && <h1>Error....</h1>} */}
+        {isError ? (
+          <p className="container"> there is error ...</p> //using the ternary operators
+        ) : (
+          <div>
+            <h2>There is no error</h2>{" "}
+          </div>
+        )}
+        <h1>{!text || "we are desperate people"}</h1>
+        <button className="btn" onClick={increase}>
+          Plus 1
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ShortCircuit;

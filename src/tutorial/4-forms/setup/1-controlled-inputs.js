b165889c0f65aed81ad8a477503b554f8582b68c
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-02 02:29:19 +0100

    ref state value

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -10,17 +10,32 @@ const ControlledInputs = () => {
     e.preventDefault();
     console.log("Hello Nigerians");
   };
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <article>
       {/* //onSubmit={handleSubmit} */}
       <form className="form">
         <div className="form-control">
           <label htmlFor="firstName">Name:</label>
-          <input className="firstName" id="firstName" name="firstName" />
+          <input
+            className="firstName"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email:</label>
-          <input className="email" id="email" name="email" />
+          <input
+            className="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <button className="btn" type="submit" onClick={handleSubmit}>
           add Person

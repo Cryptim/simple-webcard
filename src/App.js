d9b120b7d64aa2646493b733dc8c2999623c94a1
Timothy Ignatius Agbor <67354557+Cryptim@users.noreply.github.com>
2023-05-03 13:50:14 +0100

    useReducer-state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Setup from "./tutorial/6-useReducer/setup/index";
+import Setup from "./tutorial/6-useReducer/setup";
+import Final from "./tutorial/6-useReducer/final/index";
 function App() {
   return (
     <div className="container">
       <Setup />
+      {/* <Final /> */}
     </div>
   );
 }
